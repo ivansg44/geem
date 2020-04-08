e3958ed0e1d0445abe4d07e4193c7ae89da3c1a8
Ivan Gill <ivan.gill@bccdc.ca>
2020-04-08 13:51:16 -0700

    Expand available parsing expressions

--- scripts/parsing_expressions.py
+++ scripts/parsing_expressions.py
@@ -4,7 +4,41 @@ import argparse
 import re
 
 parsing_exps = {
-    'day - integer': '(?P<day_int>[123]?\\d)'
+    'day - integer':
+        '(?P<day_int>[123]?\\d)',
+    'day - 2 digit':
+        '(?P<day_dd>[0123]\\d)',
+    'weekday - integer':
+        '(?P<weekday_int>[1-7])',
+    'weekday - 3 char':
+        '(?P<weekday_abbr>(mon|tue|wed|thu|fri|sat|sun))',
+    'weekday - word':
+        '(?P<weekday_word>'
+        '(monday|tuesday|wednesday|thursday|friday|saturday|sunday))',
+    'month - integer':
+        '(?P<month_int>\\d?\\d)',
+    'month - 2 digit':
+        '(?P<month_mm>\\d\\d)',
+    'month - 3 char':
+        '(?P<month_abbr>(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec))',
+    'month - word':
+        '(?P<month_word>'
+        '(january|february|march|april|may|june|july'
+        '|august|september|october|november|december))',
+    'year - 4 digit':
+        '(?P<year_yyyy>\\d{4})',
+    'year - 2 digit':
+        '(?P<year_yy>\\d\\d)',
+    'year - 19yy':
+        '(?P<year_yy>\\d\\d)',
+    'date - iso 8601':
+        '(?P<year_yyyy>\\d{4})-(?P<month_mm>\\d\\d)-(?P<day_dd>\\d?\\d)',
+    'Date - mm/dd/yy':
+        '(?P<month_mm>\\d{4})/(?P<day_dd>\\d\\d)/(?P<year_yy>\\d?\\d)',
+    'integer':
+        '(?P<integer>\\d+)',
+    'decimal':
+        '(?P<integer>\\d+)(?P<fractional>\\.\\d+)'
 }
 
 
