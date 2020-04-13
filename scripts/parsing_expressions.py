d1455365e8219cebb2c44c1e30ac9b32ba079718
Ivan Gill <ivan.gill@bccdc.ca>
2020-04-13 16:12:25 -0700

    Add unit information to expressions dict

--- scripts/parsing_expressions.py
+++ scripts/parsing_expressions.py
@@ -6,56 +6,96 @@ import argparse
 import re
 
 parsing_exps = {
-    'day - integer':
-        r'^(?P<day_int>[1-9]|[12][0-9]|30|31)$',
-    'day - 2 digit':
-        r'^(?P<day_dd>0[1-9]|[12][0-9]|30|31)$',
-    'weekday - integer':
-        r'^(?P<weekday_int>[1-7])$',
-    'weekday - 3 char':
-        r'^(?P<weekday_abbr>(mon|tue|wed|thu|fri|sat|sun))$',
-    'weekday - word':
-        r'^(?P<weekday_word>'
-        r'(monday|tuesday|wednesday|thursday|friday|saturday|sunday))$',
-    'month - integer':
-        r'^(?P<month_int>[1-9]|10|11|12)$',
-    'month - 2 digit':
-        r'^(?P<month_mm>0[1-9]|10|11|12)$',
-    'month - 3 char':
-        r'^(?P<month_abbr>'
-        r'(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec))$',
-    'month - word':
-        r'^(?P<month_word>'
-        r'(january|february|march|april|may|june|july'
-        r'|august|september|october|november|december))$',
-    'year - 4 digit':
-        r'^(?P<year_yyyy>\d{4})$',
-    'year - 2 digit':
-        r'^(?P<year_yy>\d\d)$',
-    'date - iso 8601':
-        r'^(?P<year_yyyy>\d{4})'
-        r'-(?P<month_mm>0[1-9]|10|11|12)'
-        r'-(?P<day_dd>0[1-9]|[12][0-9]|30|31)$',
-    'date - mm/dd/yy':
-        r'^(?P<month_mm>0[1-9]|10|11|12)'
-        r'/(?P<day_dd>0[1-9]|[12][0-9]|30|31)'
-        r'/(?P<year_yy>\d\d)$',
-    'integer':
-        r'^(?P<integer>\d+)$',
-    'decimal':
-        r'^(?P<integer>\d+)(?P<fractional>\.\d+)$',
-    'boolean 1/0':
-        r'^(?P<boolean_10>[01])$',
-    'boolean y/n':
-        r'^(?P<boolean_yn>[yn])$',
-    'boolean yes/no':
-        r'^(?P<boolean_yes_no>yes|no)$'
+    'day - integer': {
+        'exp': r'^(?P<day_int>[1-9]|[12][0-9]|30|31)$',
+        'unit': 'day',
+        'unit_uri': 'UO_0000033'
+    },
+    'day - 2 digit': {
+        'exp': r'^(?P<day_dd>0[1-9]|[12][0-9]|30|31)$',
+        'unit': 'day',
+        'unit_uri': 'UO_0000033'
+    },
+    'weekday - integer': {
+        'exp': r'^(?P<weekday_int>[1-7])$',
+        'unit': 'day',
+        'unit_uri': 'UO_0000033'
+    },
+    'weekday - 3 char': {
+        'exp': r'^(?P<weekday_abbr>(mon|tue|wed|thu|fri|sat|sun))$',
+        'unit': 'day',
+        'unit_uri': 'UO_0000033'
+    },
+    'weekday - word': {
+        'exp': r'^(?P<weekday_word>'
+               r'(monday|tuesday|wednesday|thursday|friday|saturday|sunday))$',
+        'unit': 'day',
+        'unit_uri': 'UO_0000033'
+    },
+    'month - integer': {
+        'exp': r'^(?P<month_int>[1-9]|10|11|12)$',
+        'unit': 'month',
+        'unit_uri': 'UO_0000035'
+    },
+    'month - 2 digit': {
+        'exp': r'^(?P<month_mm>0[1-9]|10|11|12)$',
+        'unit': 'month',
+        'unit_uri': 'UO_0000035'
+    },
+    'month - 3 char': {
+        'exp': r'^(?P<month_abbr>'
+               r'(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec))$',
+        'unit': 'month',
+        'unit_uri': 'UO_0000035'
+    },
+    'month - word': {
+        'exp': r'^(?P<month_word>'
+               r'(january|february|march|april|may|june|july'
+               r'|august|september|october|november|december))$',
+        'unit': 'month',
+        'unit_uri': 'UO_0000035'
+    },
+    'year - 4 digit': {
+        'exp': r'^(?P<year_yyyy>\d{4})$',
+        'unit': 'year',
+        'unit_uri': 'UO_0000036'
+    },
+    'year - 2 digit': {
+        'exp': r'^(?P<year_yy>\d\d)$',
+        'unit': 'year',
+        'unit_uri': 'UO_0000036'
+    },
+    'date - iso 8601': {
+        'exp': r'^(?P<year_yyyy>\d{4})'
+               r'-(?P<month_mm>0[1-9]|10|11|12)'
+               r'-(?P<day_dd>0[1-9]|[12][0-9]|30|31)$',
+    },
+    'date - mm/dd/yy': {
+        'exp': r'^(?P<month_mm>0[1-9]|10|11|12)'
+               r'/(?P<day_dd>0[1-9]|[12][0-9]|30|31)'
+               r'/(?P<year_yy>\d\d)$',
+    },
+    'integer': {
+        'exp': r'^(?P<integer>\d+)$',
+    },
+    'decimal': {
+        'exp': r'^(?P<integer>\d+)(?P<fractional>\.\d+)$',
+    },
+    'boolean 1/0': {
+        'exp': r'^(?P<boolean_10>[01])$',
+    },
+    'boolean y/n': {
+        'exp': r'^(?P<boolean_yn>[yn])$',
+    },
+    'boolean yes/no': {
+        'exp': r'^(?P<boolean_yes_no>yes|no)$'
+    }
 }
 
 
 def main(exp, col):
     """TODO: document function"""
-    compiled_exp = re.compile(parsing_exps[exp], re.IGNORECASE)
+    compiled_exp = re.compile(parsing_exps[exp]['exp'], re.IGNORECASE)
 
     # e.g. ``{'month_mm': [], 'day_dd': [], 'year_yy': []}``
     col_components = {x: [] for x in compiled_exp.groupindex}
