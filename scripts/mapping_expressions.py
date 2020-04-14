855761d06540d6fc739a2fd11410e5f6e343b85d
Ivan Gill <ivan.gill@bccdc.ca>
2020-04-14 12:47:13 -0700

    Several changes
    * Store parsing expressions in JSON
    
      * Loaded via ``get_parsing_exps``
    
    * Move more code to ``main()``
    
    * Parse args in ``parse_args``
    
    * Pass JSON value for exp to ``get_col_components`` instead of exp's
      name
    
    * Adjusted tests accordingly
    
    * Commented out ``map_col`` for now

--- scripts/mapping_expressions.py
+++ scripts/mapping_expressions.py
@@ -3,99 +3,33 @@
 """TODO: document script"""
 
 import argparse
+import json
 import re
+import os
 
-parsing_exps = {
-    'day - integer': {
-        'exp': r'^(?P<day_int>[1-9]|[12][0-9]|30|31)$',
-        'unit': 'day',
-        'unit_uri': 'UO_0000033'
-    },
-    'day - 2 digit': {
-        'exp': r'^(?P<day_dd>0[1-9]|[12][0-9]|30|31)$',
-        'unit': 'day',
-        'unit_uri': 'UO_0000033'
-    },
-    'weekday - integer': {
-        'exp': r'^(?P<weekday_int>[1-7])$',
-        'unit': 'day',
-        'unit_uri': 'UO_0000033'
-    },
-    'weekday - 3 char': {
-        'exp': r'^(?P<weekday_abbr>(mon|tue|wed|thu|fri|sat|sun))$',
-        'unit': 'day',
-        'unit_uri': 'UO_0000033'
-    },
-    'weekday - word': {
-        'exp': r'^(?P<weekday_word>'
-               r'(monday|tuesday|wednesday|thursday|friday|saturday|sunday))$',
-        'unit': 'day',
-        'unit_uri': 'UO_0000033'
-    },
-    'month - integer': {
-        'exp': r'^(?P<month_int>[1-9]|10|11|12)$',
-        'unit': 'month',
-        'unit_uri': 'UO_0000035'
-    },
-    'month - 2 digit': {
-        'exp': r'^(?P<month_mm>0[1-9]|10|11|12)$',
-        'unit': 'month',
-        'unit_uri': 'UO_0000035'
-    },
-    'month - 3 char': {
-        'exp': r'^(?P<month_abbr>'
-               r'(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec))$',
-        'unit': 'month',
-        'unit_uri': 'UO_0000035'
-    },
-    'month - word': {
-        'exp': r'^(?P<month_word>'
-               r'(january|february|march|april|may|june|july'
-               r'|august|september|october|november|december))$',
-        'unit': 'month',
-        'unit_uri': 'UO_0000035'
-    },
-    'year - 4 digit': {
-        'exp': r'^(?P<year_yyyy>\d{4})$',
-        'unit': 'year',
-        'unit_uri': 'UO_0000036'
-    },
-    'year - 2 digit': {
-        'exp': r'^(?P<year_yy>\d\d)$',
-        'unit': 'year',
-        'unit_uri': 'UO_0000036'
-    },
-    'date - iso 8601': {
-        'exp': r'^(?P<year_yyyy>\d{4})'
-               r'-(?P<month_mm>0[1-9]|10|11|12)'
-               r'-(?P<day_dd>0[1-9]|[12][0-9]|30|31)$',
-    },
-    'date - mm/dd/yy': {
-        'exp': r'^(?P<month_mm>0[1-9]|10|11|12)'
-               r'/(?P<day_dd>0[1-9]|[12][0-9]|30|31)'
-               r'/(?P<year_yy>\d\d)$',
-    },
-    'integer': {
-        'exp': r'^(?P<integer>\d+)$',
-    },
-    'decimal': {
-        'exp': r'^(?P<integer>\d+)(?P<fractional>\.\d+)$',
-    },
-    'boolean 1/0': {
-        'exp': r'^(?P<boolean_10>[01])$',
-    },
-    'boolean y/n': {
-        'exp': r'^(?P<boolean_yn>[yn])$',
-    },
-    'boolean yes/no': {
-        'exp': r'^(?P<boolean_yes_no>yes|no)$'
-    }
-}
+from definitions import BASE_DIR
 
 
-def get_col_components(col, exp):
+def get_parsing_exps():
     """TODO: document function"""
-    compiled_exp = re.compile(parsing_exps[exp]['exp'], re.IGNORECASE)
+    parsing_exps_json_path = \
+        os.path.join(BASE_DIR, 'scripts', 'data', 'parsing_exps.json')
+    with open(parsing_exps_json_path) as fp:
+        return json.load(fp)
+
+
+def parse_args(parsing_exps):
+    """TODO: document function"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('parsing_expression', choices=parsing_exps.keys())
+    parser.add_argument('column', nargs='*')
+    parser.add_argument('--mapping_expression', choices=parsing_exps.keys())
+    return parser.parse_args()
+
+
+def get_col_components(col, parsing_exp):
+    """TODO: document function"""
+    compiled_exp = re.compile(parsing_exp['exp'], re.IGNORECASE)
 
     # e.g. ``{'month_mm': [], 'day_dd': [], 'year_yy': []}``
     col_components = {x: [] for x in compiled_exp.groupindex}
@@ -113,31 +47,30 @@ def get_col_components(col, exp):
 
 def map_col(col_components, exp):
     """TODO: document function"""
-    compiled_exp = re.compile(parsing_exps[exp]['exp'], re.IGNORECASE)
-
-    # Iterate over parameters in ``exp``.
-    # e.g., ``month_mm``, ``day_dd``, ``year_yy``
-    for expected_component in compiled_exp.groupindex.keys():
-        # Raise an error if a parameter is missing from
-        # ``col_components``.
-        if expected_component not in col_components:
-            msg = 'This column type cannot be converted into ' + exp
-            raise argparse.ArgumentTypeError(msg)
+    # compiled_exp = re.compile(parsing_exps[exp]['exp'], re.IGNORECASE)
+    #
+    # # Iterate over parameters in ``exp``.
+    # # e.g., ``month_mm``, ``day_dd``, ``year_yy``
+    # for expected_component in compiled_exp.groupindex.keys():
+    #     # Raise an error if a parameter is missing from
+    #     # ``col_components``.
+    #     if expected_component not in col_components:
+    #         msg = 'This column type cannot be converted into ' + exp
+    #         raise argparse.ArgumentTypeError(msg)
 
     return []
 
 
-def main(args_):
-    col_components = get_col_components(args_.column, args_.parsing_expression)
-    if args_.mapping_expression:
-        return map_col(col_components, args_.mapping_expression)
+def main():
+    parsing_exps = get_parsing_exps()
+    args = parse_args(parsing_exps)
+
+    col_components = \
+        get_col_components(args.column, parsing_exps[args.parsing_expression])
+    if args.mapping_expression:
+        return map_col(col_components, args.mapping_expression)
     return col_components
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('parsing_expression', choices=parsing_exps.keys())
-    parser.add_argument('column', nargs='*')
-    parser.add_argument('--mapping_expression', choices=parsing_exps.keys())
-    args = parser.parse_args()
-    main(args)
+    main()
--- tests/test_scripts/test_mapping_expressions.py
+++ tests/test_scripts/test_mapping_expressions.py
@@ -9,23 +9,26 @@ from scripts import mapping_expressions
 
 
 class TestGetColComponents(unittest.TestCase):
+    parsing_exps = mapping_expressions.get_parsing_exps()
+
     def test_empty_col(self):
         col = []
+        parsing_exp = self.parsing_exps['day - integer']
         expected = {'day_int': []}
-        actual = mapping_expressions.get_col_components(col, 'day - integer')
+        actual = mapping_expressions.get_col_components(col, parsing_exp)
         self.assertDictEqual(expected, actual)
 
     def test_case_insensitivity(self):
         col = [
             'mOn'
         ]
+        parsing_exp = self.parsing_exps['weekday - 3 char']
         expected = {
             'weekday_abbr': [
                 'mOn'
             ]
         }
-        actual = \
-            mapping_expressions.get_col_components(col, 'weekday - 3 char')
+        actual = mapping_expressions.get_col_components(col, parsing_exp)
         self.assertDictEqual(expected, actual)
 
     def test_day_integer(self):
@@ -33,13 +36,14 @@ class TestGetColComponents(unittest.TestCase):
             '', 'ham', '0', '1', '2', '9', '99', '10', '11', '19', '20',
             '2020', '30', '3030', '31', '3131'
         ]
+        parsing_exp = self.parsing_exps['day - integer']
         expected = {
             'day_int': [
                 None, None, None, '1', '2', '9', None, '10', '11', '19', '20',
                 None, '30', None, '31', None
             ]
         }
-        actual = mapping_expressions.get_col_components(col, 'day - integer')
+        actual = mapping_expressions.get_col_components(col, parsing_exp)
         self.assertDictEqual(expected, actual)
 
     def test_day_2_digit(self):
@@ -47,13 +51,14 @@ class TestGetColComponents(unittest.TestCase):
             '', 'ham', '1', '0', '01', '02', '09', '0909', '10', '11',
             '19', '20', '2020', '30', '3030', '31', '3131'
         ]
+        parsing_exp = self.parsing_exps['day - 2 digit']
         expected = {
             'day_dd': [
                 None, None, None, None, '01', '02', '09', None, '10', '11',
                 '19', '20', None, '30', None, '31', None
             ]
         }
-        actual = mapping_expressions.get_col_components(col, 'day - 2 digit')
+        actual = mapping_expressions.get_col_components(col, parsing_exp)
         self.assertDictEqual(expected, actual)
         self.assertDictEqual(expected, actual)
 
@@ -61,38 +66,39 @@ class TestGetColComponents(unittest.TestCase):
         col = [
             '', 'ham', '0', '1', '2', '7', '8', '77'
         ]
+        parsing_exp = self.parsing_exps['weekday - integer']
         expected = {
             'weekday_int': [
                 None, None, None, '1', '2', '7', None, None
             ]
         }
-        actual = \
-            mapping_expressions.get_col_components(col, 'weekday - integer')
+        actual = mapping_expressions.get_col_components(col, parsing_exp)
         self.assertDictEqual(expected, actual)
 
     def test_weekday_3_char(self):
         col = [
             '', 'ham', 'mon', 'wed', 'wedwed'
         ]
+        parsing_exp = self.parsing_exps['weekday - 3 char']
         expected = {
             'weekday_abbr': [
                 None, None, 'mon', 'wed', None
             ]
         }
-        actual = \
-            mapping_expressions.get_col_components(col, 'weekday - 3 char')
+        actual = mapping_expressions.get_col_components(col, parsing_exp)
         self.assertDictEqual(expected, actual)
 
     def test_weekday_word(self):
         col = [
             '', 'ham', 'monday', 'wednesday', 'wednesdaywednesday'
         ]
+        parsing_exp = self.parsing_exps['weekday - word']
         expected = {
             'weekday_word': [
                 None, None, 'monday', 'wednesday', None
             ]
         }
-        actual = mapping_expressions.get_col_components(col, 'weekday - word')
+        actual = mapping_expressions.get_col_components(col, parsing_exp)
         self.assertDictEqual(expected, actual)
 
     def test_month_integer(self):
@@ -100,13 +106,14 @@ class TestGetColComponents(unittest.TestCase):
             '', 'ham', '0', '1', '2', '9', '99', '10', '1010', '11', '1111',
             '12', '1212'
         ]
+        parsing_exp = self.parsing_exps['month - integer']
         expected = {
             'month_int': [
                 None, None, None, '1', '2', '9', None, '10', None, '11', None,
                 '12', None
             ]
         }
-        actual = mapping_expressions.get_col_components(col, 'month - integer')
+        actual = mapping_expressions.get_col_components(col, parsing_exp)
         self.assertDictEqual(expected, actual)
         self.assertDictEqual(expected, actual)
 
@@ -115,61 +122,66 @@ class TestGetColComponents(unittest.TestCase):
             '', 'ham', '0', '1', '00', '01', '02', '09', '0909', '10', '1010',
             '11', '1111', '12', '1212'
         ]
+        parsing_exp = self.parsing_exps['month - 2 digit']
         expected = {
             'month_mm': [
                 None, None, None, None, None, '01', '02', '09', None, '10',
                 None, '11', None, '12', None
             ]
         }
-        actual = mapping_expressions.get_col_components(col, 'month - 2 digit')
+        actual = mapping_expressions.get_col_components(col, parsing_exp)
         self.assertDictEqual(expected, actual)
 
     def test_month_3_char(self):
         col = [
             '', 'ham', 'jan', 'may', 'maymay'
         ]
+        parsing_exp = self.parsing_exps['month - 3 char']
         expected = {
             'month_abbr': [
                 None, None, 'jan', 'may', None
             ]
         }
-        actual = mapping_expressions.get_col_components(col, 'month - 3 char')
+        actual = mapping_expressions.get_col_components(col, parsing_exp)
         self.assertDictEqual(expected, actual)
 
     def test_month_word(self):
         col = [
             '', 'ham', 'january', 'may', 'maymay'
         ]
+        parsing_exp = self.parsing_exps['month - word']
         expected = {
             'month_word': [
                 None, None, 'january', 'may', None
             ]
         }
-        actual = mapping_expressions.get_col_components(col, 'month - word')
+        actual = mapping_expressions.get_col_components(col, parsing_exp)
         self.assertDictEqual(expected, actual)
 
     def test_year_4_digit(self):
         col = [
             '', 'ham', '1', '11', '111', '1111', '0000', '9999', '99999'
         ]
+        parsing_exp = self.parsing_exps['year - 4 digit']
         expected = {
             'year_yyyy': [
                 None, None, None, None, None, '1111', '0000', '9999', None
             ]
         }
-        actual = mapping_expressions.get_col_components(col, 'year - 4 digit')
+        actual = mapping_expressions.get_col_components(col, parsing_exp)
         self.assertDictEqual(expected, actual)
 
     def test_year_2_digit(self):
         col = [
             '', 'ham', '1', '11', '22', '00', '99', '999'
         ]
+        parsing_exp = self.parsing_exps['year - 2 digit']
         expected = {
             'year_yy': [
                 None, None, None, '11', '22', '00', '99', None
             ]
         }
-        actual = mapping_expressions.get_col_components(col, 'year - 2 digit')
+        actual = mapping_expressions.get_col_components(col, parsing_exp)
         self.assertDictEqual(expected, actual)
 
     def test_date_iso_8601(self):
@@ -183,6 +195,7 @@ class TestGetColComponents(unittest.TestCase):
             '2222-11-10', '2222-11-19', '2222-11-20', '2222-11-29',
             '2222-11-30', '2222-11-31', '2222-11-32'
         ]
+        parsing_exp = self.parsing_exps['date - iso 8601']
         expected = {
             'year_yyyy': [
                 None, None, '2222', None, None, None, None, None, None, None,
@@ -203,19 +216,20 @@ class TestGetColComponents(unittest.TestCase):
                 '29', '30', '31', None
             ]
         }
-        actual = mapping_expressions.get_col_components(col, 'date - iso 8601')
+        actual = mapping_expressions.get_col_components(col, parsing_exp)
         self.assertDictEqual(expected, actual)
 
     def test_date_mm_dd_yy(self):
         col = [
             '//', 'ham/ham/ham', '11/22/22', '112222', '11/2222',
-            '1122/22', '11/22/2', '11/22/', '11/22/222', '11/22/00', 
+            '1122/22', '11/22/2', '11/22/', '11/22/222', '11/22/00',
             '11/22/99', '1/22/2222', '/22/22', '111/22/22', '00/22/22',
             '01/22/22', '09/22/22', '12/22/22', '13/22/22', '11/2/22',
             '11//22', '11/222/22', '11/00/22', '11/01/22', '11/09/22',
             '11/10/22', '11/19/22', '11/20/22', '11/29/22', '11/30/22',
             '11/31/22', '11/32/22'
         ]
+        parsing_exp = self.parsing_exps['date - mm/dd/yy']
         expected = {
             'year_yy': [
                 None, None, '22', None, None, None, None, None, None, '00',
@@ -236,19 +250,20 @@ class TestGetColComponents(unittest.TestCase):
                 '31', None
             ]
         }
-        actual = mapping_expressions.get_col_components(col, 'date - mm/dd/yy')
+        actual = mapping_expressions.get_col_components(col, parsing_exp)
         self.assertDictEqual(expected, actual)
 
     def test_integer(self):
         col = [
             '', 'ham', '1', '11', '0', '9', '090', '090 ham'
         ]
+        parsing_exp = self.parsing_exps['integer']
         expected = {
             'integer': [
                 None, None, '1', '11', '0', '9', '090', None
             ]
         }
-        actual = mapping_expressions.get_col_components(col, 'integer')
+        actual = mapping_expressions.get_col_components(col, parsing_exp)
         self.assertDictEqual(expected, actual)
 
     def test_decimal(self):
@@ -256,6 +271,7 @@ class TestGetColComponents(unittest.TestCase):
             '', 'ham', '1', '1.', '1.1', '11.1', '1.11', '0.0', '9.9',
             '9.9 ham'
         ]
+        parsing_exp = self.parsing_exps['decimal']
         expected = {
             'integer': [
                 None, None, None, None, '1', '11', '1', '0', '9', None
@@ -264,43 +280,46 @@ class TestGetColComponents(unittest.TestCase):
                 None, None, None, None, '.1', '.1', '.11', '.0', '.9', None
             ]
         }
-        actual = mapping_expressions.get_col_components(col, 'decimal')
+        actual = mapping_expressions.get_col_components(col, parsing_exp)
         self.assertDictEqual(expected, actual)
 
     def test_boolean_1_0(self):
         col = [
             '', 'ham', '1', '0', '1 ham'
         ]
+        parsing_exp = self.parsing_exps['boolean 1/0']
         expected = {
             'boolean_10': [
                 None, None, '1', '0', None
             ]
         }
-        actual = mapping_expressions.get_col_components(col, 'boolean 1/0')
+        actual = mapping_expressions.get_col_components(col, parsing_exp)
         self.assertDictEqual(expected, actual)
 
     def test_boolean_y_n(self):
         col = [
             '', 'ham', 'y', 'n', 'yes'
         ]
+        parsing_exp = self.parsing_exps['boolean y/n']
         expected = {
             'boolean_yn': [
                 None, None, 'y', 'n', None
             ]
         }
-        actual = mapping_expressions.get_col_components(col, 'boolean y/n')
+        actual = mapping_expressions.get_col_components(col, parsing_exp)
         self.assertDictEqual(expected, actual)
 
     def test_boolean_yes_no(self):
         col = [
             '', 'ham', 'yes', 'no', 'yes ham'
         ]
+        parsing_exp = self.parsing_exps['boolean yes/no']
         expected = {
             'boolean_yes_no': [
                 None, None, 'yes', 'no', None
             ]
         }
-        actual = mapping_expressions.get_col_components(col, 'boolean yes/no')
+        actual = mapping_expressions.get_col_components(col, parsing_exp)
         self.assertDictEqual(expected, actual)
 
 
